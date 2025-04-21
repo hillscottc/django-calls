37c59c5f6753f33d9f868265e8d6800481639762
Scott Hill <scott289@gmail.com>
2025-04-21 15:10:00 -0700

    better weather

--- app/polls/views.py
+++ app/polls/views.py
@@ -53,7 +53,7 @@ def vote(request, question_id):
 
 def weather(request, zip):
 
-    # Geocoding API request
+    # Geocoding API request to get the latitude, longitude, and city for given zip code
     geocoding_url = f"https://geocoding-api.open-meteo.com/v1/search?name={zip}"
     geocoding_response = requests.get(geocoding_url)
     geocoding_data = geocoding_response.json()
@@ -61,20 +61,20 @@ def weather(request, zip):
     if geocoding_data and geocoding_data['results']:
         latitude = geocoding_data['results'][0]['latitude']
         longitude = geocoding_data['results'][0]['longitude']
-        city = geocoding_data['results'][0]['admin1']
+        admin1 = geocoding_data['results'][0]['admin1']
+        admin2 = geocoding_data['results'][0]['admin2']
 
-        # Weather API request
+        # Weather API request by lat/long
         weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}"
-        # weather_url = f"{weather_url}&forecast_days=2&hourly=temperature_2m,rain"
         weather_url = f"{weather_url}&temperature_unit=fahrenheit&current=temperature_2m,rain"
         weather_response = requests.get(weather_url)
         weather_data = weather_response.json()
 
-        # add the city to the data
-        weather_data.update({'city': city})
+        # add the location to the data
+        weather_data.update({'location': admin2 + ", " + admin1})
 
-        return JsonResponse(weather_data)
+        # return JsonResponse(weather_data)
+        response = f"In {weather_data['location']}, the current temperature is {weather_data['current']['temperature_2m']}°F, and rain is {weather_data['current']['rain']} inches."
+        return HttpResponse(response)
     else:
         return "Could not find location for that zip code."
-    # response = "You're looking at the results of weather for zip %s."
-    # return HttpResponse(response % zip)
