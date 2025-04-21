c92fbb23d4059ae55657cef84a246451374c25f5
Scott Hill <scott289@gmail.com>
2025-04-21 13:19:00 -0700

    weather call

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,9 +1,11 @@
+import requests
 from django.db.models import F
+from django.http import JsonResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.http import HttpResponse
 from .models import Choice, Question
 
 
@@ -47,3 +49,26 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def weather(request, zip):
+
+    # 1. Geocoding API request
+    geocoding_url = f"https://geocoding-api.open-meteo.com/v1/search?name={zip}"
+    geocoding_response = requests.get(geocoding_url)
+    geocoding_data = geocoding_response.json()
+
+    if geocoding_data and geocoding_data['results']:
+        latitude = geocoding_data['results'][0]['latitude']
+        longitude = geocoding_data['results'][0]['longitude']
+
+        # 2. Weather API request
+        weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,rain"
+        weather_response = requests.get(weather_url)
+        weather_data = weather_response.json()
+        # return weather_data
+        return JsonResponse(weather_data)
+    else:
+        return "Could not find location for that zip code."
+    # response = "You're looking at the results of weather for zip %s."
+    # return HttpResponse(response % zip)
