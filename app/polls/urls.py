27bb1dc18de41df8f318269bcff0167246c7b2cf
Scott Hill <scott289@gmail.com>
2025-04-23 12:22:47 -0700

    url cleanup

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("async/", views.my_async_view, name="my_async_view"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("", views.async_user_list, name="async_user_list"),
+    path("users/", views.UsersView.as_view(), name="user_list"),
     path("weather/<int:zip>/", views.weather, name="weather"),
     path("send_all/", views.send_all, name="send_all"),
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -12,35 +12,25 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "user_list"
-
-    def get_queryset(self):
-        # logger.warning("HELLO")
-        # logger.info("HELLO")
-        # logger.debug("HELLO")
-        """Return first 50 Users"""
-        return User.objects.all()[:50]
-
-
 @sync_to_async
 def get_users():
-    return list(
-        User.objects.all()
-    )
+    return list(User.objects.all())
 
 
-async def my_async_view(request):
+async def async_user_list(request):
     logger.info("Rendering user list page.")
     users = await get_users()
     context = {"users": users}
-    return render(request, "polls/my-async.html", context)
+    return render(request, "polls/index.html", context)
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+class UsersView(generic.ListView):
+    template_name = "polls/users.html"
+    context_object_name = "user_list"
+
+    def get_queryset(self):
+        """Return first 50 Users"""
+        return User.objects.all()[:50]
 
 
 def weather(request, zip):
@@ -60,26 +50,3 @@ async def send_all(request):
 
     context = {"results": results}
     return render(request, "polls/send-all-results.html", context)
-
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(
-#             request,
-#             "polls/detail.html",
-#             {
-#                 "question": question,
-#                 "error_message": "You didn't select a choice.",
-#             },
-#         )
-#     else:
-#         selected_choice.votes = F("votes") + 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
