e34160e688cc9a1d61db4671613dbee632007a8d
Scott Hill <scott289@gmail.com>
2025-05-05 16:16:07 -0700

    adjusted zip for 5 digits

--- app/weathapp/models.py
+++ app/weathapp/models.py
@@ -15,18 +15,18 @@ def validate_phone(value):
 
 
 def validate_zip(value):
-    """Verify zip code format is like 12345 or 12345-6789"""
-    if not re.match(r"^\d{5}(-\d{4})?$", value):
+    """Verify zip code format is 5 digits"""
+    if not re.match(r"^\d{5}$", value):
         raise ValidationError(
-            _("%(value)s is not valid zip code. Must be like 12345 or 12345-6789"),
+            _("%(value)s is not valid zip code. Must be five digit number"),
             params={"value": value},
         )
 
 
 class AppUser(models.Model):
     name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200, validators=[validate_phone])
-    zip = models.CharField(max_length=10, validators=[validate_zip])
+    phone = models.CharField(max_length=12, validators=[validate_phone])
+    zip = models.CharField(max_length=5, validators=[validate_zip])
 
     def __str__(self):
         return f"{self.name}, ({self.phone}), {self.zip}"
